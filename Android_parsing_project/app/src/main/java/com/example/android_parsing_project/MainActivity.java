d61e99db5c8b5098c6bb0b05d4d3cf6b8f368aa4
Dong <idh1007@naver.com>
2022-02-14 13:40:11 +0900

    android study

--- Android_parsing_project/app/src/main/java/com/example/android_parsing_project/MainActivity.java
+++ Android_parsing_project/app/src/main/java/com/example/android_parsing_project/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-s
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
