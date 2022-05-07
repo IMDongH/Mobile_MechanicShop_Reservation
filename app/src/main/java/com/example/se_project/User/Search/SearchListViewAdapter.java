e8aca24567b79907603094f76124bed2dec1fe82
dong <idh1007@naver.com>
2022-05-08 00:21:22 +0900

    검색창 구현중

---
+++ app/src/main/java/com/example/se_project/User/Search/SearchListViewAdapter.java
@@ -0,0 +1,113 @@
+package com.example.se_project.User.Search;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+import com.example.se_project.R;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class SearchListViewAdapter extends BaseAdapter {
+
+    // Declare Variables
+
+    Context mContext;
+    LayoutInflater inflater;
+    private ArrayList<SearchTitleClass> titlesList;
+    private ArrayList<SearchTitleClass> arraylist;
+    RelativeLayout itemList;
+
+    public SearchListViewAdapter(Context context, ArrayList<SearchTitleClass> titlesList) {
+        mContext = context;
+        this.titlesList = new ArrayList<SearchTitleClass>();
+        inflater = LayoutInflater.from(mContext);
+        this.arraylist = new ArrayList<SearchTitleClass>();
+        this.arraylist.addAll(titlesList);
+        Log.e("adapter", "ListViewAdapter: " + arraylist.size());
+    }
+
+    public class ViewHolder {
+        TextView Name;
+        TextView Location;
+    }
+
+    @Override
+    public int getCount() {
+
+        return titlesList.size();
+    }
+
+    @Override
+    public SearchTitleClass getItem(int position) {
+        return titlesList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(final int position, View view, ViewGroup parent) {
+        final ViewHolder holder;
+        if (view == null) {
+            holder = new ViewHolder();
+            view = inflater.inflate(R.layout.activity_search_list_view_items, null);
+            // Locate the TextViews in listview_item.xml
+            holder.Name = (TextView) view.findViewById(R.id.Name_name);
+            holder.Location = (TextView) view.findViewById(R.id.Location_name);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        // Set the results into TextViews
+        System.out.println("HOLDER : "+titlesList.get(position).getName()+titlesList.get(position).getLocation());
+        if(!titlesList.isEmpty())
+        {
+
+            holder.Name.setText(titlesList.get(position).getName());
+            holder.Location.setText(titlesList.get(position).getLocation());
+        }
+        itemList = (RelativeLayout) view.findViewById(R.id.search_item);
+        itemList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //띄워주는 부분
+            }
+        });
+        return view;
+    }
+
+    // Filter Class
+    public void filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        // 새 텍스트가 추가됐으니 일단 기존의 리스트를 비우고 일치하는 단어로 이루어진 리스트를 새로 만든다.
+
+        // 입력된 텍스트의 길이가 0이면(입력이 안됐으면) 다시 이전의 리스트들을 띄운다.
+        if (charText.length() == 0) {
+            titlesList.clear();
+            notifyDataSetChanged();
+        } else {
+            titlesList.clear();
+            for (SearchTitleClass wp : arraylist) {
+                if (wp.getLocation().toLowerCase(Locale.getDefault()).contains(charText)||
+                        wp.getName().toLowerCase(Locale.getDefault()).contains(charText)) {
+                    titlesList.add(wp);
+                    System.out.println("test : "+wp.getName()+wp.getLocation());
+                }
+            }
+            notifyDataSetChanged();
+        }
+
+    }
+}
+
--- app/src/main/java/com/example/se_project/User/UserMainActivity.java
+++ app/src/main/java/com/example/se_project/User/UserMainActivity.java
@@ -2,23 +2,63 @@ package com.example.se_project.User;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.se_project.R;
+import com.example.se_project.User.Search.SearchListViewAdapter;
+import com.example.se_project.User.Search.SearchTitleClass;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class UserMainActivity extends AppCompatActivity {
     private long backKeyPressedTime = 0;
     private Toast terminate_guide_msg;
+    SearchListViewAdapter adapter;
+    ListView list;
+    SearchView searchView;
+    ArrayList<SearchTitleClass> arraylist = new ArrayList<SearchTitleClass>();
+    java.util.HashMap<String,Object> HashMap = new HashMap<String,Object>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.hide();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collectionGroup("brand").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                    HashMap = (HashMap<String, Object>) documentSnapshot.getData();
+                    String info = (String) HashMap.get("info");
+                    String title = (String) HashMap.get("title");
+                    SearchTitleClass stc = new SearchTitleClass(title, info);
+                    arraylist.add(stc);
+                    System.out.println("TEST STC : " + stc);
+                }
+                list = findViewById(R.id.listview);
+                adapter = new SearchListViewAdapter(this,arraylist);
+                list.setAdapter(adapter);
+
+                // Locate the EditText in listview_main.xml
+            }
+
+        });
+
         StartActivity(UserMapActivity.class);
+
+
     }
 
     @Override
@@ -39,6 +79,7 @@ public class UserMainActivity extends AppCompatActivity {
         }
 
     }
+
     private void StartActivity(Class c) {
         Intent intent = new Intent(this, c);
         // 동일한 창이 여러번 뜨게 만드는 것이 아니라 기존에 켜져있던 창을 앞으로 끌어와주는 기능.
@@ -47,4 +88,32 @@ public class UserMainActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.SearchMenu);
+        searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("검색어를 입력하세요.");
+
+        searchView.setOnQueryTextListener(queryTextListener);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+            // 텍스트 입력 후 검색 버튼이 눌렸을 때의 이벤트
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            adapter.filter(newText);
+            return false;
+        }
+    };
 }
