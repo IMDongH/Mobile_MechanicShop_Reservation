a1d40f1a993a75d0c33520cf0a0877c5b45c3359
dong <idh1007@naver.com>
2022-05-08 22:20:03 +0900

    .

--- app/src/main/java/com/example/se_project/Center/CenterMainActivity.java
+++ app/src/main/java/com/example/se_project/Center/CenterMainActivity.java
@@ -15,7 +15,7 @@ public class CenterMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_center_main);
+        setContentView(R.layout.activity_user_main);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
     }
