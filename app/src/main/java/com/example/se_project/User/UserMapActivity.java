28d26bc5442429bbf2c5473ac440f31832667d8a
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-05-09 20:30:45 +0900

    카센터 예약화면 미완성

--- app/src/main/java/com/example/se_project/User/UserMapActivity.java
+++ app/src/main/java/com/example/se_project/User/UserMapActivity.java
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.se_project.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -89,6 +91,11 @@ public class UserMapActivity extends AppCompatActivity implements OnMapReadyCall
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
                                     Log.d("TAG","reserve pressed");
+                                    Bundle dataBundle = new Bundle();
+                                    dataBundle.putString("centerName",marker.getTitle());
+                                    dataBundle.putString("centerAddress",marker.getSnippet());
+                                    Intent intent = new Intent(getApplicationContext(),UserCarCenterReservationActivity.class);
+                                    startActivity(intent);
                                     dialogInterface.dismiss();
                                 }
                             });
@@ -129,5 +136,18 @@ public class UserMapActivity extends AppCompatActivity implements OnMapReadyCall
         });
 
 
+    }
+
+    private void StartActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        // 동일한 창이 여러번 뜨게 만드는 것이 아니라 기존에 켜져있던 창을 앞으로 끌어와주는 기능.
+        // 이 플래그를 추가하지 않을 경우 창들이 중복돼서 계속 팝업되게 된다.
+        // 메인화면을 띄우는 모든 코드에서 이 플래그를 추가해줘야 하는 것 같다.
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    private void StartToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
