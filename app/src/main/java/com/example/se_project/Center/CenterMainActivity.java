3be59ad023df939d0f8ecc1ea328cc4deb2680bf
lib26 <dladlsqja@naver.com>
2022-05-12 23:33:20 +0900

    로그아웃

--- app/src/main/java/com/example/se_project/Center/CenterMainActivity.java
+++ app/src/main/java/com/example/se_project/Center/CenterMainActivity.java
@@ -1,17 +1,28 @@
 package com.example.se_project.Center;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.se_project.LoginActivity;
 import com.example.se_project.R;
+import com.example.se_project.User.Search.UserSearchActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CenterMainActivity extends AppCompatActivity {
+
     private long backKeyPressedTime = 0;
     private Toast terminate_guide_msg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +31,42 @@ public class CenterMainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_menu_center, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.SearchMenu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.LogoutMenu:
+                FirebaseAuth.getInstance().signOut();
+                StartActivity(LoginActivity.class);
+                break;
+
+            case R.id.SettingMenu:
+                Toast.makeText(getApplicationContext(), "설정 메뉴", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void StartActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        // 동일한 창이 여러번 뜨게 만드는 것이 아니라 기존에 켜져있던 창을 앞으로 끌어와주는 기능.
+        // 이 플래그를 추가하지 않을 경우 창들이 중복돼서 계속 팝업되게 된다.
+        // 메인화면을 띄우는 모든 코드에서 이 플래그를 추가해줘야 하는 것 같다.
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 
     @Override
