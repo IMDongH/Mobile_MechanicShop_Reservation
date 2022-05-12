ea718aa45411de8b9800194eab5e7c97a5771876
lib26 <dladlsqja@naver.com>
2022-05-13 07:23:12 +0900

    - 센터 회원가입시 미리 넣어놨던 데이터에 전화번호 추가 (동혁 요청사항) - 엑션바 날짜선택, 로그아웃

--- app/src/main/java/com/example/se_project/Center/CenterMainActivity.java
+++ app/src/main/java/com/example/se_project/Center/CenterMainActivity.java
@@ -46,6 +46,7 @@ public class CenterMainActivity extends AppCompatActivity {
         // selected_Date 정보를 이용하여 예약 정보를 DB 에서 가져오고 리스트뷰 어뎁터에 넘겨준다
         // db.collection(아마 reservation).get. 해서 문서 다가오고 -> 센터이름 && 날짜 일치하면 -> 어뎁터에 정보 넘기고 -> 화면에 리스트뷰 뿌리면 될듯
         // 센터 이름은 db.collection(enterprises).document(users.getUid()).get().addOnCompleteListener 해서 document.get("centerName")로 가져오면 될듯
+        // test
     }
 
     // 선택한 날짜를 myCalendar 에 설정해준다
